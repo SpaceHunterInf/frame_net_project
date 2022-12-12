76d2743ccf3fb4ba223059bc5d6a108e6780ee35
spacehunterinf <xiaochen.zhu@hhu.de>
2022-12-12 19:58:08 +0000

    added lots of exception handling for corrupted data and simplfied verb matching

--- batch_projection.py
+++ batch_projection.py
@@ -5,13 +5,14 @@ from copy import deepcopy
 from tqdm import tqdm
 from utils import *
 import os, re, sys
-import joblib
+import joblib, json
 import logging
+import stopit
 logging.basicConfig(filename='projection_log.txt',
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
-                    level=logging.DEBUG)
+                    level=logging.WARNING)
 
 logging.info("Running Urban Planning")
 
@@ -45,7 +46,8 @@ def update_graph_old(sentence, eds:EDS, semlink:SemLinkAnnotation):
     if source_verb_node == None:
         print(target_verb)
         print(sentence)
-        raise Exception('empty verb???')
+        return 'pass','pass','pass','pass'
+        #raise Exception('empty verb???')
     updated_predicate = source_verb_node.predicate + '-fn.' + semlink.fn_frame
 
     #update edge roles
@@ -90,40 +92,47 @@ def update_graph_old(sentence, eds:EDS, semlink:SemLinkAnnotation):
     
     return source_verb_node.id, updated_predicate, source_verb_edges_dict
 
-def update_graph(sentence, eds:EDS, semlink:SemLinkAnnotation):
+def update_graph(sentence, eds:EDS, semlink:SemLinkAnnotation, remaining_verb_nodes):
     tokens = tokenize(semlink)
     token_idx = int(semlink.token_no)
     target_verb = tokens[token_idx]
     begin_idx = len(detokenize(tokens[:token_idx]))
     end_idx = len(detokenize(tokens[:token_idx+1]))
-    candidate_verb_nodes = get_verb_nodes(eds)
+    candidate_verb_nodes = deepcopy(remaining_verb_nodes)
     
     source_verb_node = None
     #update predicate structure
     for verb_node in candidate_verb_nodes:
-        s,e = verb_node.lnk.data
-        # print(s,e)
-        idx_pairs = sorted([(s,e), (begin_idx, end_idx)], key= lambda x : x[0])
+        # s,e = verb_node.lnk.data
+        # # print(s,e)
+        # idx_pairs = sorted([(s,e), (begin_idx, end_idx)], key= lambda x : x[0])
 
-        if idx_pairs[0][1] > idx_pairs[1][0]:
-            #overlap confirmed
+        # if idx_pairs[0][1] > idx_pairs[1][0]:
+        #     #overlap confirmed
+        #     source_verb_node = verb_node
+        if semlink.verb in verb_node.predicate:
             source_verb_node = verb_node
-    
+            candidate_verb_nodes.remove(verb_node)
+            break
     # print(begin_idx, end_idx)
     # print(source_verb_node)
     if source_verb_node == None:
-        print(target_verb)
-        print(sentence)
-        raise Exception('empty verb???')
+        # print(target_verb)
+        # print("SENTENCE : " + sentence)
+        # print(get_file_name(semlink))
+        logging.warning(get_file_name(semlink))
+        logging.warning('no matching verb')
+        return 'pass','pass','pass','pass'
+        #raise Exception('empty verb???')
     updated_predicate = source_verb_node.predicate + '-fn.' + semlink.fn_frame
 
     #update edge roles
-    target_dependencies = [x for x in semlink.dependencies if ';' in x]
+    target_dependencies = [x for x in semlink.dependencies if ';' in x.split('-')[-1]]
     arg_assosicated_strings = get_children_strings(sentence, eds, source_verb_node)
 
     source_verb_edges_dict = deepcopy(source_verb_node.edges)
     for dependency in target_dependencies:
-        token_intervals = dependency.split('-')[0].replace(';','*').split('*')
+        token_intervals = dependency.split('-')[0].replace(';','*').replace(',','*').split('*')
         concate_string = ""
         for interval in token_intervals:
             start_token_idx = int(interval.split(':')[0])
@@ -149,14 +158,14 @@ def update_graph(sentence, eds:EDS, semlink:SemLinkAnnotation):
             new_key = old_key + '-fn.' + fn_role
             arg_assosicated_strings.pop(old_key)
             source_verb_edges_dict[new_key] = source_verb_edges_dict.pop(old_key)
-            print(new_key)
+            #print(new_key)
         except:
             #exit() annotation mismatch
-            logging.warning('annotation mismatch', edit_distance, source_verb_node, source_verb_node.edges, sentence, semlink.file_path)
+            logging.warning('annotation mismatch', edit_distances, source_verb_node, source_verb_node.edges, sentence, semlink.file_path)
 
         
     
-    return source_verb_node.id, updated_predicate, source_verb_edges_dict
+    return source_verb_node.id, updated_predicate, source_verb_edges_dict, candidate_verb_nodes
 
 if __name__ == "__main__":
     # semlinks_data = [line.rstrip() for line in open('1.2.2c.okay.txt')]
@@ -179,12 +188,42 @@ if __name__ == "__main__":
     with open('semlink_dict.pkl', 'rb') as f:
         semlinks_dict = joblib.load(f)
 
+    total_unmatch = 0
+    total_cannot_load = 0
+    total_timeout = 0
+    output_dict = {}
     for f in tqdm(semlinks_dict.keys(), desc='Processing Files'):
-        current_deepbank = sentence_eds(os.path.join('deepbank_raw',f))
-        for semlink in semlinks_dict[f]:
-            new_id, new_pred, new_edges = update_graph(current_deepbank.sentence, current_deepbank.eds, semlink)
-            current_deepbank.update_text(new_id, new_pred, new_edges)
+        skip_flag = False
+        try:
+            current_deepbank = sentence_eds(os.path.join('deepbank_raw',f))
+        except:
+            logging.warning('Cannot Load {}'.format(f))
+            total_cannot_load += 1
+            skip_flag = True
+        if skip_flag == False:
+            verb_nodes = get_verb_nodes(current_deepbank.eds)
+            for semlink in semlinks_dict[f]:
+                if not(verb_nodes == 'pass'):
+                    with stopit.ThreadingTimeout(2) as context_manager:
+                        new_id, new_pred, new_edges, verb_nodes = update_graph(current_deepbank.sentence, current_deepbank.eds, semlink, verb_nodes)
+                    if context_manager.state == context_manager.EXECUTED:
+                        if not(verb_nodes == 'pass'):
+                            current_deepbank.update_text(new_id, new_pred, new_edges)
+                        else:
+                            total_unmatch += 1
+                    elif context_manager.state == context_manager.TIMED_OUT:
+                        logging.warning('Possible Loop in {}'.format(f))
+                        total_timeout +=1
+
+            output_dict[f] = current_deepbank.eds_text
+
+    
+    print(total_unmatch, total_cannot_load, total_timeout)
+    with open(os.path.join('deepbank_projected', 'projected.pkl'), 'wb') as f:
+        joblib.dump(output_dict, f)
+
+    with open(os.path.join('deepbank_projected', 'projected.json'), 'w') as f:
+        f.write(json.dumps(output_dict))
+        f.close()
         
-        with open(os.path.join('deepbank_projected', f+'_projected'), 'w') as f:
-            f.write(current_deepbank.eds_text)
-            f.close()
+    
